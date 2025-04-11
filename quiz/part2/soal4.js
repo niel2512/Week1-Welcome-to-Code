5e552b6fdb66501cced863eb34b7618d497236d0
niel2512 <nielyusuf17@gmail.com>
2025-04-11 10:47:45 +0700

    Soal1.js Done

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,9 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+let word4 = "wow JavaScript is so cool";
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log("First Word: " + exampleFirstWord + ", with length: " + firstWordLength);
